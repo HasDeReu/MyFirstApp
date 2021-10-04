4fafeb160e27bdc9ca4a9787360851de214ddec9
Hogye Denis Ruben <h_denix@yahoo.com>
2021-10-04 08:54:38 +0300

    Versiunea1

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace MyFirstApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello 123 World!");
         }
     }
 }
